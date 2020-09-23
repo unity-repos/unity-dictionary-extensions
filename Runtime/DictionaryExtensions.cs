132f9fb13ae83b6077b585bc8d6f5c71f9b4fa3f
DaiCapra <daniel@artmann.co>
2020-09-23 17:59:05 +0200

    first commit

---
+++ Runtime/DictionaryExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class DictionaryExtensions
+{
+    public static void Set<TK, TV>(this Dictionary<TK, TV> dict, TK key, TV value)
+    {
+        if (dict == null)
+        {
+            return;
+        }
+
+        if (!dict.ContainsKey(key))
+        {
+            dict.Add(key, value);
+        }
+        else
+        {
+            dict[key] = value;
+        }
+    }
+
+    public static bool TryGet<TK, TV>(this Dictionary<TK, TV> dict, TK key, out TV value)
+    {
+        value = default;
+        if (dict == null || !dict.ContainsKey(key))
+        {
+            return false;
+        }
+
+        value = dict[key];
+
+        return true;
+    }
+}
